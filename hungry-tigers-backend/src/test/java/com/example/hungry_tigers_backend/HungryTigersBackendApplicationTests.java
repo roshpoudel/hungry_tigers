8a33e32b90d76b2fb4ec954fc729d336ab0c123f
Roshpdl <pdlroshan0@gmail.com>
2024-10-03 12:40:48 -0500

    initial commit

---
+++ hungry-tigers-backend/src/test/java/com/example/hungry_tigers_backend/HungryTigersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hungry_tigers_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryTigersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
